7846c537c54aaaaab55531ce75950e780a3215a8
trentindev <trentindev@gmail.com>
2022-12-01 14:53:59 +0100

    create question form

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\QuestionType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/question', name: 'question_')]
-class QuestionController extends AbstractController {
+class QuestionController extends AbstractController
+{
   #[Route('/ask', name: 'form')]
-  public function index(): Response {
+  public function index(Request $request): Response
+  {
+
+    $formQuestion = $this->createForm(QuestionType::class);
+
+    $formQuestion->handleRequest($request);
+
+    if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
+      dump($formQuestion->getData());
+    }
+
     return $this->render('question/index.html.twig', [
-      'controller_name' => 'QuestionController',
+      'form' => $formQuestion->createView(),
     ]);
   }
 }

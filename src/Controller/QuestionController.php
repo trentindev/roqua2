a9497a998cd63cb4e9375172379e020f3e23f240
Laurent Dislaire <siladire@free.fr>
2022-12-12 22:52:08 +0100

    Form question type and controller - ending by bdd test OK

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Form\QuestionType;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -12,15 +15,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class QuestionController extends AbstractController
 {
   #[Route('/ask', name: 'form')]
-  public function index(Request $request): Response
+  public function index(Request $request, EntityManagerInterface $em): Response
   {
 
-    $formQuestion = $this->createForm(QuestionType::class);
+    $question =new Question();   
+    
+    $formQuestion = $this->createForm(QuestionType::class,$question);
 
     $formQuestion->handleRequest($request);
 
     if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
-      dd($formQuestion->getData());
+      $question->setNbrOfResponse(0);
+      $question->setRating(0);
+      $question->setCreatedAt(new \DateTimeImmutable());
+      $em->persist($question);
+      $em->flush();
+      $this->addFlash('success', 'Votre question a été ajoutée');
+      return $this->redirectToRoute('home');
+
+
     }
 
     return $this->render('question/index.html.twig', [
--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +21,7 @@ class QuestionType extends AbstractType
   public function configureOptions(OptionsResolver $resolver)
   {
     $resolver->setDefaults([
-      // Configure your form options here
+      'data_class' => Question::class
     ]);
   }
 }

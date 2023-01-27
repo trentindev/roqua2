9dd4d01c72f2798c0a0f5c4073591045948c7273
Laurent Dislaire <siladire@free.fr>
2023-01-27 17:16:00 +0100

    Setting User inscription

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
+
+class SecurityController extends AbstractController
+{
+
+    public function __construct(
+        private FormLoginAuthenticator $authenticator
+    ) {
+    }
+
+    #[Route('/signup', name: 'signup')]
+    public function signup(UserAuthenticatorInterface $userAuthenticator, Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+    {
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $hash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Bienvenue sur Wonder !');
+
+            return $userAuthenticator->authenticateUser($user, $this->authenticator, $request);
+        }
+        return $this->render('security/signup.html.twig', ['form' => $userForm->createView()]);
+    }
+
+
+    #[Route("/login", name: "login")]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route("/logout", name: "logout")]
+    public function logout()
+    {
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,211 +8,197 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
-{
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+class User implements UserInterface, PasswordAuthenticatedUserInterface {
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 180, unique: true)]
-    private ?string $email = null;
+  #[ORM\Column(length: 180, unique: true)]
+  #[Assert\Email(message: 'Veuillez rentrer un email valide.')]
+  #[Assert\NotBlank(message: 'Veuillez renseigner votre email.')]
+  private ?string $email = null;
 
-    #[ORM\Column]
-    private array $roles = [];
+  #[ORM\Column]
+  private array $roles = [];
 
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    private ?string $password = null;
+  /**
+   * @var string The hashed password
+   */
+  #[ORM\Column]
+  #[Assert\Length(min: 6, minMessage: 'Le mot de passe doit faire au moins 6 caractères.')]
+  #[Assert\NotBlank(message: 'Veuillez renseigner un mot de passe.')]
+  private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $firstname = null;
+  #[ORM\Column(length: 255)]
+  #[Assert\NotBlank(message: 'Veuillez renseigner votre prénom.')]
+  private ?string $firstname = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $lastname = null;
+  #[ORM\Column(length: 255)]
+  #[Assert\NotBlank(message: 'Veuillez renseigner votre nom.')]
+  private ?string $lastname = null;
 
-    #[ORM\OneToMany(mappedBy: 'author', targetEntity: Question::class, orphanRemoval: true)]
-    private Collection $questions;
+  #[ORM\OneToMany(mappedBy: 'author', targetEntity: Question::class, orphanRemoval: true)]
+  private Collection $questions;
 
-    #[ORM\OneToMany(mappedBy: 'author', targetEntity: Comment::class, orphanRemoval: true)]
-    private Collection $comments;
+  #[ORM\OneToMany(mappedBy: 'author', targetEntity: Comment::class, orphanRemoval: true)]
+  private Collection $comments;
 
-    #[ORM\Column(length: 255)]
-    private ?string $picture = null;
+  #[ORM\Column(length: 255)]
+  #[Assert\Url(message: 'Veuillez renseigner une url.')]
+  #[Assert\NotBlank(message: 'Veuillez renseigner une image de profil.')]
+  private ?string $picture = null;
 
-    public function __construct()
-    {
-        $this->questions = new ArrayCollection();
-        $this->comments = new ArrayCollection();
+  public function __construct() {
+    $this->questions = new ArrayCollection();
+    $this->comments = new ArrayCollection();
+  }
+
+  public function getId(): ?int {
+    return $this->id;
+  }
+
+  public function getEmail(): ?string {
+    return $this->email;
+  }
+
+  public function setEmail(string $email): self {
+    $this->email = $email;
+
+    return $this;
+  }
+
+  /**
+   * A visual identifier that represents this user.
+   *
+   * @see UserInterface
+   */
+  public function getUserIdentifier(): string {
+    return (string) $this->email;
+  }
+
+  /**
+   * @see UserInterface
+   */
+  public function getRoles(): array {
+    $roles = $this->roles;
+    // guarantee every user at least has ROLE_USER
+    $roles[] = 'ROLE_USER';
+
+    return array_unique($roles);
+  }
+
+  public function setRoles(array $roles): self {
+    $this->roles = $roles;
+
+    return $this;
+  }
+
+  /**
+   * @see PasswordAuthenticatedUserInterface
+   */
+  public function getPassword(): string {
+    return $this->password;
+  }
+
+  public function setPassword(string $password): self {
+    $this->password = $password;
+
+    return $this;
+  }
+
+  /**
+   * @see UserInterface
+   */
+  public function eraseCredentials() {
+    // If you store any temporary, sensitive data on the user, clear it here
+    // $this->plainPassword = null;
+  }
+
+  public function getFirstname(): ?string {
+    return $this->firstname;
+  }
+
+  public function setFirstname(string $firstname): self {
+    $this->firstname = $firstname;
+
+    return $this;
+  }
+
+  public function getLastname(): ?string {
+    return $this->lastname;
+  }
+
+  public function setLastname(string $lastname): self {
+    $this->lastname = $lastname;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Question>
+   */
+  public function getQuestions(): Collection {
+    return $this->questions;
+  }
+
+  public function addQuestion(Question $question): self {
+    if (!$this->questions->contains($question)) {
+      $this->questions->add($question);
+      $question->setAuthor($this);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeQuestion(Question $question): self {
+    if ($this->questions->removeElement($question)) {
+      // set the owning side to null (unless already changed)
+      if ($question->getAuthor() === $this) {
+        $question->setAuthor(null);
+      }
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Comment>
+   */
+  public function getComments(): Collection {
+    return $this->comments;
+  }
+
+  public function addComment(Comment $comment): self {
+    if (!$this->comments->contains($comment)) {
+      $this->comments->add($comment);
+      $comment->setAuthor($this);
     }
 
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
+    return $this;
+  }
 
-        return $this;
+  public function removeComment(Comment $comment): self {
+    if ($this->comments->removeElement($comment)) {
+      // set the owning side to null (unless already changed)
+      if ($comment->getAuthor() === $this) {
+        $comment->setAuthor(null);
+      }
     }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->email;
-    }
+    return $this;
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+  public function getPicture(): ?string {
+    return $this->picture;
+  }
 
-        return array_unique($roles);
-    }
+  public function setPicture(string $picture): self {
+    $this->picture = $picture;
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
-
-    public function getFirstname(): ?string
-    {
-        return $this->firstname;
-    }
-
-    public function setFirstname(string $firstname): self
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): self
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Question>
-     */
-    public function getQuestions(): Collection
-    {
-        return $this->questions;
-    }
-
-    public function addQuestion(Question $question): self
-    {
-        if (!$this->questions->contains($question)) {
-            $this->questions->add($question);
-            $question->setAuthor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeQuestion(Question $question): self
-    {
-        if ($this->questions->removeElement($question)) {
-            // set the owning side to null (unless already changed)
-            if ($question->getAuthor() === $this) {
-                $question->setAuthor(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Comment>
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments->add($comment);
-            $comment->setAuthor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getAuthor() === $this) {
-                $comment->setAuthor(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getPicture(): ?string
-    {
-        return $this->picture;
-    }
-
-    public function setPicture(string $picture): self
-    {
-        $this->picture = $picture;
-
-        return $this;
-    }
+    return $this;
+  }
 }
